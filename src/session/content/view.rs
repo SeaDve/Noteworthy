d40332d9f9d74df5120c45e38a0bc7566a414b0d
SeaDve <davecruz48@gmail.com>
2021-09-06 19:06:21 +0800

    Add todo

--- src/session/content/view.rs
+++ src/session/content/view.rs
@@ -64,6 +64,7 @@ mod imp {
             let modified_str_expr = gtk::ClosureExpression::new(
                 |args| {
                     let date: Date = args[1].get().unwrap();
+                    // TODO use fuzzy here
                     format!("Last edited {}", date)
                 },
                 &[modified_expression],
