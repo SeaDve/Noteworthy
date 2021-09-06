7a11fc6e12d7bb5fbfd188710f3ac79d4d1d4935
SeaDve <davecruz48@gmail.com>
2021-09-06 08:59:26 +0800

    Fix broken date

--- src/date.rs
+++ src/date.rs
@@ -33,7 +33,7 @@ impl Date {
         let now = Local::now();
         let is_today = now.date() == self.0.date();
 
-        let duration = self.0.signed_duration_since(now);
+        let duration = now.signed_duration_since(self.0);
         let hours_difference = duration.num_hours();
         let week_difference = duration.num_weeks();
 
