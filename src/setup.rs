9e8b0ef1ea58483de905958289058e4b83ab2dd7
SeaDve <davecruz48@gmail.com>
2021-09-28 22:30:53 +0800

    Restructure setup

--- src/setup.rs
+++ src/setup.rs
@@ -14,23 +14,34 @@ mod imp {
 
     #[derive(Debug, Default, CompositeTemplate)]
     #[template(resource = "/io/github/seadve/Noteworthy/ui/setup.ui")]
-    pub struct Setup {}
+    pub struct Setup {
+        #[template_child]
+        pub stack: TemplateChild<gtk::Stack>,
+        #[template_child]
+        pub select_provider: TemplateChild<gtk::Box>,
+    }
 
     #[glib::object_subclass]
     impl ObjectSubclass for Setup {
         const NAME: &'static str = "NwtySetup";
         type Type = super::Setup;
-        type ParentType = gtk::Box;
+        type ParentType = adw::Bin;
 
         fn class_init(klass: &mut Self::Class) {
             Self::bind_template(klass);
 
+            // TODO consider changing this action name
             klass.install_action("setup.setup-session", None, move |obj, _, _| {
                 let ctx = glib::MainContext::default();
                 ctx.spawn_local(clone!(@weak obj => async move {
                     obj.setup_session().await;
                     obj.emit_by_name("session-setup-done", &[]).unwrap();
                 }));
+            });
+
+            klass.install_action("setup.setup-git-host", None, move |obj, _, _| {
+                let imp = imp::Setup::from_instance(obj);
+                imp.stack.set_visible_child(&imp.select_provider.get());
             });
         }
 
@@ -50,12 +61,12 @@ mod imp {
     }
 
     impl WidgetImpl for Setup {}
-    impl BoxImpl for Setup {}
+    impl BinImpl for Setup {}
 }
 
 glib::wrapper! {
     pub struct Setup(ObjectSubclass<imp::Setup>)
-        @extends gtk::Widget, gtk::Box;
+        @extends gtk::Widget, adw::Bin;
 }
 
 impl Setup {
