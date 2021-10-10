9a6dff149ce381b63d7da50f74ece925bdd89893
SeaDve <davecruz48@gmail.com>
2021-10-10 16:54:32 +0800

    Setup ui

--- src/setup.rs
+++ src/setup.rs
@@ -7,7 +7,7 @@ use gtk::{
     CompositeTemplate,
 };
 
-use crate::{repository::Repository, utils};
+use crate::utils;
 
 mod imp {
     use super::*;
@@ -16,15 +16,15 @@ mod imp {
     #[template(resource = "/io/github/seadve/Noteworthy/ui/setup.ui")]
     pub struct Setup {
         #[template_child]
+        pub navigate_back_button: TemplateChild<gtk::Button>,
+        #[template_child]
+        pub navigate_forward_button: TemplateChild<gtk::Button>,
+        #[template_child]
         pub content: TemplateChild<adw::Leaflet>,
         #[template_child]
-        pub welcome: TemplateChild<gtk::Box>,
+        pub welcome: TemplateChild<adw::StatusPage>,
         #[template_child]
-        pub select_provider: TemplateChild<gtk::Box>,
-        #[template_child]
-        pub repo_url_entry: TemplateChild<gtk::Entry>,
-        #[template_child]
-        pub passphrase_entry: TemplateChild<gtk::Entry>,
+        pub select_provider: TemplateChild<adw::StatusPage>,
     }
 
     #[glib::object_subclass]
@@ -36,6 +36,16 @@ mod imp {
         fn class_init(klass: &mut Self::Class) {
             Self::bind_template(klass);
 
+            klass.install_action("setup.navigate-back", None, move |obj, _, _| {
+                let imp = imp::Setup::from_instance(obj);
+                imp.content.navigate(adw::NavigationDirection::Back);
+            });
+
+            klass.install_action("setup.navigate-forward", None, move |obj, _, _| {
+                let imp = imp::Setup::from_instance(obj);
+                imp.content.navigate(adw::NavigationDirection::Forward);
+            });
+
             // TODO consider changing these action names
             klass.install_action("setup.setup-session", None, move |obj, _, _| {
                 let ctx = glib::MainContext::default();
@@ -45,35 +55,30 @@ mod imp {
                 }));
             });
 
-            klass.install_action("setup.go-back-welcome", None, move |obj, _, _| {
-                let imp = imp::Setup::from_instance(obj);
-                imp.content.set_visible_child(&imp.welcome.get());
-            });
-
             klass.install_action("setup.setup-git-host", None, move |obj, _, _| {
                 let imp = imp::Setup::from_instance(obj);
                 imp.content.set_visible_child(&imp.select_provider.get());
             });
 
-            klass.install_action("setup.enter-repo-url", None, move |obj, _, _| {
-                let imp = imp::Setup::from_instance(obj);
-                let repo_url = imp.repo_url_entry.text();
-                let passphrase = imp.passphrase_entry.text();
+            // klass.install_action("setup.enter-repo-url", None, move |obj, _, _| {
+            //     let imp = imp::Setup::from_instance(obj);
+            //     let repo_url = imp.repo_url_entry.text();
+            //     let passphrase = imp.passphrase_entry.text();
 
-                let ctx = glib::MainContext::default();
-                ctx.spawn_local(async move {
-                    let repo_path = gio::File::for_path(&glib::user_data_dir());
-                    let repo = Repository::new(&repo_path);
-                    if let Err(err) = repo
-                        .clone(repo_url.to_string(), passphrase.to_string())
-                        .await
-                    {
-                        log::error!("Failed to clone: {}", err);
-                    } else {
-                        log::info!("Successfull repo clone");
-                    }
-                });
-            });
+            //     let ctx = glib::MainContext::default();
+            //     ctx.spawn_local(async move {
+            //         let repo_path = gio::File::for_path(&glib::user_data_dir());
+            //         let repo = Repository::new(&repo_path);
+            //         if let Err(err) = repo
+            //             .clone(repo_url.to_string(), passphrase.to_string())
+            //             .await
+            //         {
+            //             log::error!("Failed to clone: {}", err);
+            //         } else {
+            //             log::info!("Successfull repo clone");
+            //         }
+            //     });
+            // });
         }
 
         fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
@@ -88,6 +93,16 @@ mod imp {
                 vec![Signal::builder("session-setup-done", &[], <()>::static_type().into()).build()]
             });
             SIGNALS.as_ref()
+        }
+
+        fn constructed(&self, obj: &Self::Type) {
+            self.content
+                .connect_visible_child_notify(clone!(@weak obj => move |content| {
+                    let imp = imp::Setup::from_instance(&obj);
+                    let is_main_page = content.visible_child() == Some(imp.welcome.get().upcast());
+                    imp.navigate_back_button.set_visible(!is_main_page);
+                    imp.navigate_forward_button.set_visible(!is_main_page);
+                }));
         }
     }
 
