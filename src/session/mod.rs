0f1ff84196de2f97157729b42ad1f1bc20df638b
SeaDve <davecruz48@gmail.com>
2021-10-12 11:09:16 +0800

    Add fixme to broken tag list

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -136,6 +136,9 @@ mod imp {
 
                 let imp = imp::Session::from_instance(&obj);
                 imp.sidebar.set_note_list(note_manager.note_list());
+
+                // FIXME do binding here because we update the tag list again
+                // or maybe find other workarounds
                 imp.sidebar.set_tag_list(note_manager.tag_list());
 
                 note_manager.update().await.expect("Failed to update notes and data file");
