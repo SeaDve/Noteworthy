327a091a2edb590732ebc545d66ca1ed4e096136
SeaDve <davecruz48@gmail.com>
2021-10-21 19:45:27 +0800

    Add ssh_key

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,5 +2,6 @@ mod note_manager;
 mod note_repository;
 mod repository;
 mod repository_watcher;
+mod ssh_key;
 
-pub use self::{note_manager::NoteManager, note_repository::NoteRepository};
+pub use self::{note_manager::NoteManager, note_repository::NoteRepository, ssh_key::SshKey};
