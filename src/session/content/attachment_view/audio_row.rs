64c90d9ad5ee79e2d7be0e4af35eb4d0aca2b286
SeaDve <davecruz48@gmail.com>
2021-11-08 21:50:31 +0800

    Reduce seek stabilization time from 50ms to 20ms

--- src/session/content/attachment_view/audio_row.rs
+++ src/session/content/attachment_view/audio_row.rs
@@ -212,7 +212,7 @@ impl AudioRow {
     fn on_playback_position_scale_value_changed(&self, scale: &gtk::Scale) {
         let imp = imp::AudioRow::from_instance(self);
 
-        // Cancel the seek when the value changed again within 50ms. So, it
+        // Cancel the seek when the value changed again within 20ms. So, it
         // will only seek when the value is stabilized within that span.
         if let Some(source_id) = imp.seek_timeout_id.take() {
             glib::source_remove(source_id); // TODO replace with `source_id.remove();` on gtk-rs 0.3.4
@@ -222,7 +222,7 @@ impl AudioRow {
 
         imp.seek_timeout_id
             .replace(Some(glib::timeout_add_local_once(
-                Duration::from_millis(50),
+                Duration::from_millis(20),
                 clone!(@weak self as obj => move || {
                     let imp = imp::AudioRow::from_instance(&obj);
                     imp.seek_timeout_id.replace(None);
