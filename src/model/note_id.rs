aa4668b87226d180e657be314b79d630886ef363
SeaDve <davecruz48@gmail.com>
2021-12-14 13:34:27 +0800

    test

--- src/model/note_id.rs
+++ src/model/note_id.rs
@@ -1,9 +1,9 @@
-use std::{ffi::OsStr, path::Path};
+use std::path::Path;
 
 // TODO optimize this (Reduce size of id in generating unique file name in utils.rs)
 #[derive(Hash, PartialEq, Eq)]
 pub struct NoteId {
-    id: Box<OsStr>,
+    id: Box<str>,
 }
 
 impl std::fmt::Debug for NoteId {
@@ -13,9 +13,9 @@ impl std::fmt::Debug for NoteId {
 }
 
 impl NoteId {
-    pub fn from_path(path: impl AsRef<Path>) -> Self {
+    pub fn from_path(path: &Path) -> Self {
         Self {
-            id: Box::from(path.as_ref().file_stem().unwrap()),
+            id: path.file_stem().unwrap().to_str().unwrap().into(),
         }
     }
 }
@@ -29,17 +29,20 @@ mod test {
     fn hash_map() {
         let mut hash_map = HashMap::new();
 
-        let id_0 = NoteId::from_path("Path0");
+        let id_0 = NoteId::from_path(&Path::new("Path0"));
         hash_map.insert(&id_0, 0);
 
-        let id_1 = NoteId::from_path("Path1");
+        let id_1 = NoteId::from_path(&Path::new("Path1"));
         hash_map.insert(&id_1, 1);
 
-        let id_2 = NoteId::from_path("Path2");
+        let id_2 = NoteId::from_path(&Path::new("Path2"));
         hash_map.insert(&id_2, 2);
 
         assert_eq!(hash_map.get(&id_0), Some(&0));
         assert_eq!(hash_map.get(&id_1), Some(&1));
-        assert_eq!(hash_map.get(&NoteId::from_path("Path2")), Some(&2));
+        assert_eq!(
+            hash_map.get(&NoteId::from_path(&Path::new("Path2"))),
+            Some(&2)
+        );
     }
 }
