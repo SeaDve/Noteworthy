a02896e4373804b5b55521ab46028ee7046cf6b2
SeaDve <davecruz48@gmail.com>
2022-01-22 19:58:23 +0800

    Script cleaning

--- build-aux/meson_post_install.py
+++ build-aux/meson_post_install.py
@@ -3,12 +3,12 @@
 from os import environ, path
 from subprocess import call
 
-if not environ.get('DESTDIR', ''):
-    PREFIX = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
-    DATA_DIR = path.join(PREFIX, 'share')
-    print('Updating icon cache...')
-    call(['gtk4-update-icon-cache', '-qtf', path.join(DATA_DIR, 'icons/hicolor')])
+if not environ.get("DESTDIR", ""):
+    PREFIX = environ.get("MESON_INSTALL_PREFIX", "/usr/local")
+    DATA_DIR = path.join(PREFIX, "share")
+    print("Updating icon cache...")
+    call(["gtk4-update-icon-cache", "-qtf", path.join(DATA_DIR, "icons/hicolor")])
     print("Compiling new schemas...")
-    call(["glib-compile-schemas", path.join(DATA_DIR, 'glib-2.0/schemas')])
+    call(["glib-compile-schemas", path.join(DATA_DIR, "glib-2.0/schemas")])
     print("Updating desktop database...")
-    call(["update-desktop-database", path.join(DATA_DIR, 'applications')])
+    call(["update-desktop-database", path.join(DATA_DIR, "applications")])
