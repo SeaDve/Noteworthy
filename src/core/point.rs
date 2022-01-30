24e3e0055c5f1dccd707e358f42822b1b6e51400
SeaDve <davecruz48@gmail.com>
2022-01-30 10:13:36 +0800

    chore: Add Point tests

--- src/core/point.rs
+++ src/core/point.rs
@@ -22,3 +22,30 @@ impl Point {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn zero() {
+        let point = Point::ZERO;
+        assert_eq!(point.x, 0.0);
+        assert_eq!(point.y, 0.0);
+    }
+
+    #[test]
+    fn new() {
+        let point = Point::new(1.0, 5.0);
+        assert_eq!(point.x, 1.0);
+        assert_eq!(point.y, 5.0);
+    }
+
+    #[test]
+    fn tuple() {
+        let point_tuple = (1.0, 5.0);
+        let point = Point::from_tuple(point_tuple);
+        assert_eq!(point.x, 1.0);
+        assert_eq!(point.y, 5.0);
+    }
+}
